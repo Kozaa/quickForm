a2f304bda4f1e5be5d72e11eed0cacc5810b1888
KacperKozanecki <88181085+KacperKozanecki@users.noreply.github.com>
2022-05-30 22:42:48 +0200

    Added chakra-ui, upgraded to react 18

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,26 @@
-import { GetServerSideProps } from "next";
-import { connectToDatabase } from "../lib/mongodb";
-export default function Home({ test }) {
-  console.log(test);
-  return <div> hey</div>;
+// import { GetServerSideProps } from "next";
+// import { connectToDatabase } from "../lib/mongodb";
+import {Container, Heading, VStack} from "@chakra-ui/react";
+
+export default function Home() {
+
+  return (
+      <Container>
+        <VStack>
+          <Heading>Welcome to QucikForm! Create simple forms with ease.</Heading>
+        </VStack>
+      </Container>
+  )
 }
 
-export async function getServerSideProps(): GetServerSideProps {
-  const { db } = await connectToDatabase();
-
-  const data = await db.collection("forms").find().toArray();
-  console.log("omg", data);
-
-  const test = JSON.parse(JSON.stringify(data));
-  // console.log("?", data);
-
-  return {
-    props: { test },
-    // props: { test: 'test' },
-  };
-}
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const { db } = await connectToDatabase();
+//
+//   const data = await db.collection("forms").find().toArray();
+//
+//   const test = JSON.parse(JSON.stringify(data));
+//
+//   return {
+//     props: { test },
+//   };
+// };
