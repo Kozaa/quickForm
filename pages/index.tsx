9d0b0e426b896c5fd0639397beecab54bfc192ba
KacperKozanecki <88181085+KacperKozanecki@users.noreply.github.com>
2022-05-30 23:37:42 +0200

    setup for hero page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 // import { GetServerSideProps } from "next";
 // import { connectToDatabase } from "../lib/mongodb";
-import {Container, Heading, VStack} from "@chakra-ui/react";
+import { Container, Heading, Text, VStack } from "@chakra-ui/react";
 
 export default function Home() {
-
   return (
-      <Container>
-        <VStack>
-          <Heading>Welcome to QucikForm! Create simple forms with ease.</Heading>
-        </VStack>
-      </Container>
-  )
+    <Container>
+      <VStack>
+        <Heading size={"4xl"}>Welcome to QuickForm!</Heading>
+        <Text>
+          Quickly compose a form that you desire and start gathering data with a
+          couple of clicks.
+        </Text>
+      </VStack>
+    </Container>
+  );
 }
 
 // export const getServerSideProps: GetServerSideProps = async () => {
