bbebf5c87952b0e33f85b2366358a6ff6f323549
KacperKozanecki <54321456+Kozaa@users.noreply.github.com>
2022-06-12 21:15:33 +0200

    git email change test

--- components/createPage/CreateMainEditor/CreateMainEditor.tsx
+++ components/createPage/CreateMainEditor/CreateMainEditor.tsx
@@ -1,4 +1,4 @@
-import { Text, VStack } from "@chakra-ui/react";
+import { Heading, Text, VStack } from "@chakra-ui/react";
 import { useContext } from "react";
 import { FormDataContext } from "../../../pages/create";
 
@@ -6,8 +6,10 @@ export function CreateMainEditor() {
   const { formData } = useContext(FormDataContext);
 
   return (
-    <VStack as="main">
-      <Text>{formData.formTitle}</Text>
+    <VStack as="main" alignItems="center" w="100%" px="30" py="10">
+      <Heading>{formData.formTitle}</Heading>
+
+
     </VStack>
   );
 }
